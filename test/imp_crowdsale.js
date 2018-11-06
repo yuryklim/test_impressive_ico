f4a790603fc5ce028d797a8888b84f8cddf94380
yuryklim <yuryklim@outlook.com>
2018-11-06 21:41:48 +0200

    test to check wei are kept on contract balance

--- test/imp_crowdsale.js
+++ test/imp_crowdsale.js
@@ -95,7 +95,7 @@ contract("IMP_Crowdsale", (accounts) => {
     });
   });
 
-  describe("minimum purchase wei value", async () => {
+  describe("purchase", async () => {
 
     const MIN_ETH = 0.00001;
     const MIN_VALUE = web3.toWei(MIN_ETH, "ether");
@@ -112,6 +112,23 @@ contract("IMP_Crowdsale", (accounts) => {
         from: ACC_1,
         value: MIN_VALUE
       });
+    });
+
+    it("wei should stay on crowdsale contract balance", async () => {
+      //  1
+      await crowdsale.sendTransaction({
+        from: ACC_1,
+        value: web3.toWei(1, "ether")
+      });
+      let balance = new BigNumber(await web3.eth.getBalance(crowdsale.address)).toNumber();
+      assert.equal(balance, new BigNumber(web3.toWei(1, "ether")).toNumber(), "wrong contract balance after purchase 1 ETH");
+      //  2
+      await crowdsale.sendTransaction({
+        from: ACC_1,
+        value: web3.toWei(2.5, "ether")
+      });
+      balance = new BigNumber(await web3.eth.getBalance(crowdsale.address)).toNumber();
+      assert.equal(balance, new BigNumber(web3.toWei(3.5, "ether")).toNumber(), "wrong contract balance after purchase 2.5 ETH");
     });
   });
 
